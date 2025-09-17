a49637d9d20f132c4a4e34650f3db6b7a530da44
Daniel Machado <danielsan.machado@gmail.com>
2025-09-17 15:14:32 -0300

    api

--- api/info.js
+++ api/info.js
@@ -1,8 +1,4 @@
-export default function handler(req, res) {
-  const { name = "mundo" } = req.query;
-  res.status(200).json({
-    ok: true,
-    message: `Olá, ${name}!`,
-    now: new Date().toISOString(),
-  });
-}
+module.exports = (req, res) => {
+  const { name = "mundo" } = req.query || {};
+  res.status(200).json({ ok: true, message: `Olá, ${name}!`, now: new Date().toISOString() });
+};
